53a7b21b309b7a8049968bda7ba8d7a2b0471de0
niraj-khatiwada <nirazzz.nk@gmail.com>
2021-02-17 14:43:55 +0545

    removed redundant

--- middleware/withProtectedRoute.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res, next) => {
-  const isAuthenticated = req.session.isAuthenticated
-
-  if (!isAuthenticated) {
-    return res.redirect('/auth/login')
-  }
-  next()
-}
--- util/database.js
+++
@@ -1,7 +0,0 @@
-const { Sequelize } = require('sequelize')
-
-const sequelize = new Sequelize('nodeJS', 'niraj', '****', {
-  dialect: 'postgres',
-})
-
-module.exports = sequelize
